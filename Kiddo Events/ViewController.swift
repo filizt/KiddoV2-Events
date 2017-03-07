2abecb253de5203e7e7602b7f099bfd0a4c66d29
Filiz Kurban <filiz@monahq.com>
2017-03-06 20:39:38 -0800

    Calidate UI fields and save event data to Parse

--- Kiddo Events/ViewController.swift
+++ Kiddo Events/ViewController.swift
@@ -9,6 +9,10 @@
 import Cocoa
 import Parse
 
+//enum Catagories: Int {
+//    case ArtsCraftsMusicSwim = 1, IndoorPlay, OutdoorPlay,"Mommy and Me","Museums","Nature/Science","Out and About","Outdoor Activity","Parent's Date Night","Shows/Concerts/Theatre","Festival and Fairs","CoffeeShop","Brewery","Others"
+//}
+
 class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
     
     @IBOutlet weak var eventTitle: NSTextField!
@@ -17,9 +21,19 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
     @IBOutlet weak var datePicker: NSDatePicker!
     @IBOutlet weak var dateListTable: NSTableView!
     @IBOutlet weak var doneButton: NSButton!
+    @IBOutlet weak var locationHours: NSTextField!
 
-    @IBOutlet weak var isPopular: NSButton!
-    @IBOutlet weak var isActive: NSButton!
+    @IBOutlet weak var eventImageObjectId: NSTextField!
+    @IBOutlet weak var popularEventCheckButton: NSButton!
+    @IBOutlet weak var eventActiveCheckButton: NSButton!
+    @IBOutlet weak var freeEventCheckButton: NSButton!
+    @IBOutlet weak var allDayCheckButton: NSButton!
+    @IBOutlet weak var eventDescription: NSTextField!
+    @IBOutlet weak var categoryList: NSPopUpButton!
+    @IBOutlet weak var eventAges: NSTextField!
+    @IBOutlet weak var eventURL: NSTextField!
+    @IBOutlet weak var locationAddress: NSTextField!
+
     var dates = [Date]() {
         didSet {
             dateListTable.reloadData()
@@ -39,14 +53,36 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
                    }
     }
 
+    var textFields = [NSTextField]()
+
+    var data = [String: Any]()
+    //To-Do: Make this an Enum later
+    let eventCategories = ["Pick One", "Arts/Crafts/Music/Swim","Indoor Play", "Outdoor Play","Mommy and Me","Museums","Nature/Science","Out and About","Outdoor Activity","Parent's Date Night","Shows/Concerts/Theatre","Festival and Fairs","CoffeeShop","Brewery","Others",]
+
     override func viewDidLoad() {
-         super.viewDidLoad()
+        super.viewDidLoad()
         datePicker.dateValue = Date()
         dateListTable.delegate = self
         dateListTable.dataSource = self
+
+        textFields.append(eventTitle)
+        textFields.append(location)
+        textFields.append(locationHours)
+        textFields.append(locationAddress)
+        textFields.append(eventPriceField)
+        textFields.append(eventAges)
+        textFields.append(eventImageObjectId)
+
+        categoryList.removeAllItems()
+        categoryList.addItems(withTitles: eventCategories)
+
+
     }
 
 
+    @IBAction func eventCategoryPicked(_ sender: NSPopUpButton) {
+        print("Pop-up category item chosen:", sender.indexOfSelectedItem)
+    }
 
     func addDate(_ sender: Any) {
         let date = datePicker.dateValue
@@ -57,12 +93,91 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
     }
 
     @IBAction func doneButtonClicked(_ sender: Any) {
-        createTestData()
-        saveTestData()
-        if validateFields() {
+        guard validateFields() else { return }
+        guard prepareData() else { return }
 
+        saveToParse()
+
+    }
+
+    private func saveToParse() {
+
+        guard data.count > 0 else { return }
+
+        let eventObject: PFObject = PFObject(className: "EventObject")
+        eventObject["title"] = data["title"]
+        eventObject["allEventDates"] = data["allEventDates"] as! [Date];
+        eventObject["startDate"] = data["startDate"] as! Date
+        eventObject["endDate"] = data["endDate"] as! Date
+        eventObject["allDay"] = data["allDay"] as! Bool
+        eventObject["startTime"] = data["startTime"] as? String
+        eventObject["endTime"] = data["endTime"] as? String
+        eventObject["free"] = data ["free"] as! Bool
+        eventObject["price"] =  data["price"] as! String
+        eventObject["originalEventURL"] = data["originalEventURL"] as! String
+        eventObject["location"] = data["location"] as! String
+        eventObject["locationHours"] = data["locationHours"] as! String
+        eventObject["address"] = data["address"] as! String
+        eventObject["description"] = data["description"] as! String
+        eventObject["ages"] = data["ages"] as! String
+        eventObject["imageURL"] = data["imageURL"] as! String
+        eventObject["isActive"] = data["isActive"] as! Bool
+        eventObject["isPopular"] = data["isPopular"] as! Bool
+        eventObject["imageObjectId"] = data["imageObjectId"] as! String
+        eventObject["category"] = data["category"] as! String
+
+        let alleventdates = data["allEventDates"] as! [Date];
+
+        //event object has all the date it needs. Save it now. In the completion handler
+        //we can check which dates it needs to have relation with.
+        guard let _ = try? eventObject.save() else { return }
+
+        for date in alleventdates {
+            //let date = alleventdates[0]
+            let q = PFQuery(className: "EventDate")
+            q.whereKey("eventDate", equalTo: date)
+            if let eventDateObjects = try? q.findObjects() {
+                if eventDateObjects.count == 0 {
+                    let dateObject: PFObject = PFObject(className: "EventDate")
+                    dateObject["eventDate"] = date
+                    let relation = dateObject.relation(forKey: "events")
+                    relation.add(eventObject)
+                    guard let _ = try? dateObject.save() else { return }
+                } else {
+                    let existingDateObject = eventDateObjects[0]
+                    let relation = existingDateObject.relation(forKey: "events")
+                    relation.add(eventObject)
+                    guard let _ = try? existingDateObject.save() else { return }
+                }
+            }
         }
+    }
 
+    private func prepareData() -> Bool {
+        data = [String: Any]()
+        data["title"] = eventTitle.stringValue
+        data["allEventDates"] = self.dates
+        data["startDate"] = Date()
+        data["endDate"] = Date()
+        data["allDay"] = allDayCheckButton.state != 0 ? true : false
+        self.dateFormatter.dateFormat = "h:mm a"
+        data["startTime"] = dateFormatter.string(from: eventStartTimePicker.dateValue)
+        data["endTime"] = dateFormatter.string(from: eventEndTimePicker.dateValue)
+        data["free"] = freeEventCheckButton.state != 0 ? true : false
+        data["price"] = eventPriceField.stringValue
+        data["originalEventURL"] = eventURL.stringValue
+        data["location"] = location.stringValue
+        data["locationHours"] = locationHours.stringValue
+        data["address"] = locationAddress.stringValue
+        data["description"] = eventDescription.stringValue
+        data["ages"] = eventAges.stringValue
+        data["imageURL"] = ""
+        data["isActive"] = eventActiveCheckButton.state != 0 ? true : false
+        data["isPopular"] = popularEventCheckButton.state != 0 ? true : false
+        data["imageObjectId"] = eventImageObjectId.stringValue
+        data["category"] = eventCategories[categoryList.indexOfSelectedItem]
+
+        return true
     }
 
     @IBAction func allDayEventPicked(_ sender: NSButton) {
@@ -91,91 +206,61 @@ class ViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSour
     }
 
     private func validateFields() -> Bool {
-        //enter validation logic here.
-        return true
-    }
-
-    private func createTestData() {
-        var data = [String: Any]()
-        data["title"] = "Test Time"
-        var allEventDates = [Date]()
-//        allEventDates.append(DateUtil.shared.createDate(from: "03-01-2017"))
-//        allEventDates.append(DateUtil.shared.createDate(from: "03-02-2017"))
-//        allEventDates.append(DateUtil.shared.createDate(from: "03-03-2017"))
-//        allEventDates.append(DateUtil.shared.createDate(from: "03-04-2017"))
-        data["allEventDates"] = self.dates
-        data["startDate"] = Date()
-        data["endDate"] = Date()
-        data["allDay"] = false
-        data["startTime"] = "9:30 AM"
-        data["endTime"] = "12:00 AM"
-        data["free"] = false
-        data["price"] = "0-3 FREE / 4 -12 16.95 / 13 and older 24.95"
-        data["originalEventURL"] = "http://www.seattleaquarium.org/"
-        data["location"] = "Seattle Aquarium"
-        data["locationHours"] = ""
-        data["address"] = "1483 Alaskan Way, Seattle, WA 98101"
-        data["description"] = "From dressing up like a wolf eel to fish-print painting and water play with ocean animal toys, Toddler Time keeps even the busiest of bodies engaged and entertained. A myriad of developmentally age-appropriate, hands-on activities await for tots to explore."
-        data["ages"] = "0 - 5"
-        data["imageURL"] = ""
-        data["imageObjectId"] = "MrXAwEQqHC"
-
-        testData["1"] =  data
-    }
-
-    func saveTestData() {
-
-        for entry in self.testData {
-            if let test = self.testData[entry.key] {
-                let eventObject: PFObject = PFObject(className: "EventObject")
-                eventObject["title"] = test["title"]
-                eventObject["allEventDates"] = test["allEventDates"] as! [Date];
-                eventObject["startDate"] = test["startDate"] as! Date
-                eventObject["endDate"] = test["endDate"] as! Date
-                eventObject["allDay"] = test["allDay"] as! Bool
-                eventObject["startTime"] = test["startTime"] as? String
-                eventObject["endTime"] = test["endTime"] as? String
-                eventObject["free"] = test ["free"] as! Bool
-                eventObject["price"] =  test["price"] as! String
-                eventObject["originalEventURL"] = test["originalEventURL"] as! String
-                eventObject["location"] = test["location"] as! String
-                eventObject["locationHours"] = test["locationHours"] as! String
-                eventObject["address"] = test["address"] as! String
-                eventObject["description"] = test["description"] as! String
-                eventObject["ages"] = test["ages"] as! String
-                eventObject["imageURL"] = test["imageURL"] as! String
-                eventObject["imageObjectId"] = test["imageObjectId"] as! String
-
-                let alleventdates = test["allEventDates"] as! [Date];
-
-                //event object has all the date it needs. Save it now. In the completion handler
-                //we can check which dates it needs to have relation with.
-                guard let _ = try? eventObject.save() else { return }
-
-                for date in alleventdates {
-                    //let date = alleventdates[0]
-                    let q = PFQuery(className: "EventDate")
-                    q.whereKey("eventDate", equalTo: date)
-                    if let eventDateObjects = try? q.findObjects() {
-                        if eventDateObjects.count == 0 {
-                            let dateObject: PFObject = PFObject(className: "EventDate")
-                            dateObject["eventDate"] = date
-                            let relation = dateObject.relation(forKey: "events")
-                            relation.add(eventObject)
-                            guard let _ = try? dateObject.save() else { return }
-                        } else {
-                            let existingDateObject = eventDateObjects[0]
-                            let relation = existingDateObject.relation(forKey: "events")
-                            relation.add(eventObject)
-                            guard let _ = try? existingDateObject.save() else { return }
-                        }
-                    }
+        var validationResult = true
+        //check textFields
+        for field in textFields {
+            if field.stringValue.isEmpty {
+                //exclude event price field, if event is free
+                if field == eventPriceField && freeEventCheckButton.state == 1 {
+                    field.backgroundColor = NSColor.white
+                    continue
                 }
 
+                validationResult = false
+                //field.layer?.borderColor = NSColor.red.cgColor
+                field.backgroundColor = NSColor.red
+            } else {
+                field.backgroundColor = NSColor.white
             }
         }
-        //  })
 
+        //check eventDescription
+        if eventDescription.stringValue.isEmpty {
+            eventDescription.backgroundColor = NSColor.red
+            validationResult = false
+        } else {
+            eventDescription.backgroundColor = NSColor.white
+
+        }
+
+        //check if table has at least one entry
+        if dates.count == 0 {
+            dateListTable.backgroundColor = NSColor.red
+            validationResult = false
+        } else {
+            dateListTable.backgroundColor = NSColor.white
+        }
+
+        //check if there is event start,end time if alldayevent flag is off.
+//        if allDayCheckButton.state == 0 {
+//            print ("checked")
+//        } else {
+//
+//            if eventStartTimePicker.dateValue.timeIntervalSinceReferenceDate * -1 > 1000  {
+//                print ("today")
+//            } else {
+//                print("Time: ", (eventStartTimePicker.dateValue.timeIntervalSinceReferenceDate * 1 ))
+//            }
+//        }
+
+        //check if event price is entered, if freeEvent flag is off
+
+        //for now category field can be empty
+        if categoryList.indexOfSelectedItem == 0 {
+             validationResult = false
+        }
+
+        return validationResult
     }
 
 
